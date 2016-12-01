c4549c0a2e14b5eeb0a507f8ca979d48911b245e
Jack Murphy <jmurphy.web@gmail.com>
2016-12-01 10:28:29 +0000

    add challenge description, and improve query for cases where no tags exist or no primary user exists

--- tests/orgs/getDetails/endpoint.test.js
+++ tests/orgs/getDetails/endpoint.test.js
@@ -22,6 +22,7 @@ test('get org details for profile view, orgs.getDetails', function (t) {
       t.deepEqual(Object.keys(ob.org), ['id', 'name', 'logo_url', 'mission_statement'], 'Gets the correct keys for the org');
       t.equal(ob.primary.first_name, 'Sally', 'Gets the correct primary user');
       t.deepEqual(Object.keys(ob.primary), [ 'first_name', 'last_name', 'id', 'phone', 'email', 'job_title'], 'Gets the correct keys for the org');
+      t.deepEqual(Object.keys(ob.challenges[0]), [ 'id', 'title', 'description', 'tags'], 'Gets the correct keys for the challenges');
       t.equal(ob.challenges.length, 2, 'Gets the correct number of challenges for org');
       t.equal(ob.challenges[0].title, 'Challenge Number 2', 'Gets the correct challenge for org');
       t.end();
