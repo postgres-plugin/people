7a46be031ce8932fb525464395962d23337be660
mk4111 <marie.kasai@live.co.uk>
2016-12-13 15:49:25 +0000

    updates test, readme and version

--- tests/people/edit/endpoint.test.js
+++ tests/people/edit/endpoint.test.js
@@ -4,26 +4,46 @@ var test = require('tape');
 var init = require('../../../example/server.js');
 var config = require('../../../config/load-config.js');
 
-// test endpoint
-test('Edit user profile', function (t) {
-  var update = {
-    first_name: 'Sally',
-    last_name: 'Robertson',
-    job_title: 'Chocolatier',
-    phone: '07111111111'
-  };
+var update = {
+  first_name: 'Sally',
+  last_name: 'Robertson',
+  job_title: 'Chocolatier',
+  phone: '07111111111'
+};
 
+function editProfile (id ) {
+  return {
+    method: 'POST',
+    url: '/peopleEdit?id=' + id,
+    payload: update
+  };
+}
+
+test('Edit user profile', function (t) {
   init(config, function (err, server, pool) {
     if (err) {
       console.log('error initialise server', err);
       return t.fail();
     }
+    server.inject(editProfile(3), function (res) {
+      t.equal(res.statusCode, 200, 'User not found, 404 returned');
+      t.equal(res.payload, '[]', 'User not found; correct message returned');
+      t.end();
+      pool.end()
+      server.stop()
+    });
+  });
+});
 
-    server.inject({
-      method: 'POST',
-      url: '/peopleEdit?id=3',
-      payload: update
-    }, function (res) {
+test('Attempt to edit non-existent user\'s profile', function (t) {
+  init(config, function (err, server, pool) {
+    if (err) {
+      console.log('error initialise server', err);
+      return t.fail();
+    }
+    server.inject(editProfile(5000), function (res) {
+      t.equal(res.statusCode, 404, 'User not found, 404 returned');
+      t.equal(res.result.message, 'The user specified does not exist', 'User not found; correct message returned');
       t.end();
       pool.end()
       server.stop()
