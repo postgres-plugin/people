ca15f9e34b44b12424fde10628a654e1885a0b41
mk4111 <marie.kasai@live.co.uk>
2016-12-21 12:19:50 +0000

    #57 - adds trigger to toggle associated users when org.active changes. Adds tests.

--- lib/fixtures/drop-tables.sql
+++ lib/fixtures/drop-tables.sql
@@ -2,3 +2,6 @@
 DROP TABLE IF EXISTS tags_organisations CASCADE;
 DROP TABLE IF EXISTS people CASCADE;
 DROP TABLE IF EXISTS organisations CASCADE;
+
+-- Delete trigger
+DROP TRIGGER IF EXISTS toggleOrgUsers on organisations;
--- tests/orgs/toggleActive/endpoint.test.js
+++ tests/orgs/toggleActive/endpoint.test.js
@@ -4,76 +4,177 @@ var test = require('tape');
 var init = require('../../../example/server.js');
 var config = require('../../../config/load-config.js');
 
+function orgsToggleActive (id) {
+  return {
+    method: 'GET',
+    url: '/orgsToggleActive?id=' + id
+  }
+}
 
-// test org with/without primary user
-// for now there is no difference, in the future we need to make any active linked primary users inactive
-test('disable an active org', function (t) {
+function orgDetails (id) {
+  return {
+    method: 'GET',
+    url: '/orgsGetDetails?id=' + id
+  }
+}
+
+function userDetails (id) {
+  return {
+    method: 'GET',
+    url: '/peopleGetById?id=' + id
+  }
+}
+
+var allUsers = {
+  method: 'GET',
+  url: '/people'
+}
+
+// test active org with active primary user
+test('disable an active org with active primary user', function (t) {
+  var orgId = 1;
+  var userId = 3;
   init(config, function (err, server, pool) {
     if (err) return t.fail('error initialising server');
 
-    server.inject({
-      method: 'GET',
-      url: '/orgsToggleActive?id=1'
-    }, function (res) {
-
-      t.deepEqual(res.result, [], 'successful disabling of org returns an empty array');
-
-      server.inject({
-        method: 'GET',
-        url: '/orgsGetDetails?id=1'
-      }, function (res) {
-        t.ok(!res.result.org.active, 'the toggle active function has disabled the previously active org');
-
-        t.end();
-        pool.end()
-        server.stop()
+    server.inject(orgDetails(orgId), function (res) {
+      t.ok(
+        res.result.org.active,
+        'the org is active to begin with'
+      );
+      server.inject(userDetails(userId), function (res) {
+        t.ok(
+          res.result[0].active,
+          'the user is active to begin with'
+        );
+        server.inject(orgsToggleActive(orgId), function (res) {
+          t.deepEqual(
+            res.result,
+            [],
+            'successful disabling of org returns an empty array'
+          );
+          server.inject(orgDetails(orgId), function (res) {
+            t.ok(
+              !res.result.org.active,
+              'previously active org has been disabled'
+            );
+            server.inject(userDetails(userId), function (res) {
+              t.ok(
+                !res.result[0].active,
+                'previously active user has also been disabled'
+              );
+              t.end();
+              pool.end()
+              server.stop()
+            });
+          });
+        });
       });
     });
   });
 });
 
 
+// test inactive org with no primary user
+test('toggle an inactive org with no primary user', function (t) {
+  var orgId = 7;
+  var preToggleUsers;
 
-// test enabling org
-test('enable an inactive org', function (t) {
   init(config, function (err, server, pool) {
     if (err) return t.fail('error initialising server');
 
-    server.inject({
-      method: 'GET',
-      url: '/orgsToggleActive?id=8'
-    }, function (res) {
-
-      t.deepEqual(res.result, [], 'successful enabling of org returns an array');
-
-      server.inject({
-        method: 'GET',
-        url: '/orgsGetDetails?id=8'
-      }, function (res) {
-        t.ok(res.result.org.active, 'the toggle active function has enabled the previously inactive org');
-
-        t.end();
-        pool.end()
-        server.stop()
+    server.inject(orgDetails(orgId), function (res) {
+      t.ok(
+        !res.result.org.active,
+        'the org is inactive to begin with'
+      );
+      server.inject(allUsers, function (res) {
+        preToggleUsers = res.result;
+        server.inject(orgsToggleActive(orgId), function (res) {
+          t.deepEqual(
+            res.result,
+            [],
+            'successful disabling of org returns an empty array'
+          );
+          server.inject(orgDetails(orgId), function (res) {
+            t.ok(
+              res.result.org.active,
+              'previously inactive org has been enabled'
+            );
+            server.inject(allUsers, function (res) {
+              t.deepEqual(
+                res.result,
+                preToggleUsers,
+                'none of the users have been affected by change in org activity'
+              );
+              t.end();
+              pool.end()
+              server.stop()
+            });
+          });
+        });
       });
     });
   });
 });
 
 
-// test disbable org with wrong id -> error?
-test('enable an inactive org', function (t) {
+// test inactive org with active user
+test('enable an inactive org with an active primary user', function (t) {
+  var orgId = 8;
+  var userId = 11;
   init(config, function (err, server, pool) {
     if (err) return t.fail('error initialising server');
 
-    server.inject({
-      method: 'GET',
-      url: '/orgsToggleActive?id=10000'
-    }, function (res) {
+    server.inject(orgDetails(orgId), function (res) {
+      t.ok(
+        !res.result.org.active,
+        'the org is inactive to begin with'
+      );
+      server.inject(userDetails(userId), function (res) {
+        t.ok(
+          res.result[0].active,
+          'the user is active to begin with'
+        );
+        server.inject(orgsToggleActive(orgId), function (res) {
+          t.deepEqual(
+            res.result,
+            [],
+            'successful enabling of org returns an empty array'
+          );
+          server.inject(orgDetails(orgId), function (res) {
+            t.ok(
+              res.result.org.active,
+              'previously inactive org is now enabled'
+            );
+            server.inject(userDetails(userId), function (res) {
+              t.ok(
+                !res.result[0].active,
+                'previously active user is not disabled'
+              );
+              t.end();
+              pool.end()
+              server.stop()
+            });
+          });
+        });
+      });
+    });
+  });
+});
 
-      t.equal(res.statusCode, 404, 'Update an organisation that doest exist returns a 404');
+// test disable org with wrong id -> error?
+test('toggle a non existent org', function (t) {
+  var orgId = 10000000;
+  init(config, function (err, server, pool) {
+    if (err) return t.fail('error initialising server');
 
-
+    server.inject(orgsToggleActive(orgId), function (res) {
+      t.equal(
+        res.statusCode,
+        404,
+        'Update an organisation that doest exist returns a 404'
+      );
       t.end();
       pool.end()
       server.stop()
