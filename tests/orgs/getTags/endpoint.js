1ecdb65ae9604d256d493919244114690228833f
simonLab <simon.labondance@gmail.com>
2017-10-06 18:04:48 +0100

    get all tags of an organistion

---
+++ tests/orgs/getTags/endpoint.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var test = require('tape');
+var init = require('../../../example/server.js');
+var config = require('../../../config/load-config.js');
+
+test('Get organistaion by id', function (t) {
+  init(config, function (err, server, pool) {
+    if (err) {
+      console.log('error initialise server', err);
+      return t.fail();
+    }
+
+    server.inject({
+      method: 'GET',
+      url: '/orgsGetTags?id=1'
+    }, function (res) {
+      t.ok(res.result.length, 2, 'The organisation with id = 1 has two tags');
+      t.end();
+      pool.end()
+      server.stop()
+    });
+  });
+});
